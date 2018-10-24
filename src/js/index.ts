48371d7f9c8e1b94b86fa8a748b5b99bc0c3b397
Frederik Skærbæk Jensen <fred15b4@edu.easj.dk>
2018-10-24 11:09:18 +0200

    Tilføjet axios kald til webservice

--- src/js/index.ts
+++ src/js/index.ts
@@ -2,10 +2,30 @@ import axios, {
     AxiosResponse, 
     AxiosError} from "../../node_modules/axios/index";
 
-    //http://rest-pele-easj-dk.azurewebsites.net/api/Cars
-
+    
     interface ICar {
         model:string;
         vendor:string;
         price:number;
+    }
+
+    let getAllCarsButton : HTMLButtonElement = <HTMLButtonElement>document.getElementById("getAllBtn");
+    getAllCarsButton.addEventListener("click", showAllCars);
+
+    function showAllCars():void{
+
+        let uri : string = "http://rest-pele-easj-dk.azurewebsites.net/api/Cars";
+
+        axios.get<ICar[]>(uri)
+        .then(function (response:AxiosResponse<ICar[]>):void{
+
+            let result : string = "<ol>";
+            response.data.forEach((car : ICar) => {
+                result += "<li>" + car.model + car.vendor + car.price.toString() + "</li>"
+            });
+
+            result += "</ol>";
+        }
+        )
+        .catch()
     }
