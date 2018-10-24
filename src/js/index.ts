80a65ac9acbca01e1acd4167e1c8870a181ba355
Frederik Skærbæk Jensen <fred15b4@edu.easj.dk>
2018-10-24 11:26:01 +0200

    Error handling er tilføjet

--- src/js/index.ts
+++ src/js/index.ts
@@ -9,23 +9,29 @@ import axios, {
         price:number;
     }
 
+    let divElement : HTMLDivElement = <HTMLDivElement>document.getElementById("content");
     let getAllCarsButton : HTMLButtonElement = <HTMLButtonElement>document.getElementById("getAllBtn");
     getAllCarsButton.addEventListener("click", showAllCars);
 
     function showAllCars():void{
 
-        let uri : string = "http://rest-pele-easj-dk.azurewebsites.net/api/Cars";
+        let uri : string = "http://rest-pele-easj-dk.azurewebsites.net/api/Cars23";
 
         axios.get<ICar[]>(uri)
         .then(function (response:AxiosResponse<ICar[]>):void{
 
             let result : string = "<ol>";
             response.data.forEach((car : ICar) => {
-                result += "<li>" + car.model + car.vendor + car.price.toString() + "</li>"
+                result += "<li>" + "Model: " + car.model + " Vendor: " + car.vendor + " Price: " + car.price.toString() + "</li>"
             });
 
             result += "</ol>";
+
+            divElement.innerHTML = result;
         }
         )
-        .catch()
+        .catch(function (error : AxiosError): void{
+
+                divElement.innerHTML = error.message;            
+        })
     }
